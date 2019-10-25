2e8cdc88846789fc861f5c25e35f7ed62536cb19
Agney Menon <agney@outlook.in>
2019-10-25 22:28:46 +0530

    style the editor

--- src/JcTransformJson.js
+++ src/JcTransformJson.js
@@ -9,6 +9,30 @@ export class JcTransformJson extends LitElement {
       :host {
         display: block;
         padding: 25px;
+        height: 100%;
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial,
+          sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
+      }
+      .editor {
+        height: 80%;
+      }
+      .title {
+        margin-top: 0;
+      }
+      .action-button {
+        width: 80%;
+        display: block;
+        margin: 0 auto;
+        background-color: rgb(59, 139, 235);
+        color: #ffffff;
+        padding: 10px;
+        margin-top: 25px;
+        border: 0;
+        box-shadow: none;
+        cursor: pointer;
+      }
+      .action-button:hover {
+        background-color: rgba(59, 139, 235, 0.9);
       }
     `;
   }
@@ -19,20 +43,35 @@ export class JcTransformJson extends LitElement {
     };
   }
 
+  constructor() {
+    super();
+    this.value = '';
+  }
+
   __transformJson() {
-    const data = JSON.stringify(this.data);
-    data.id = 1;
-    const event = new CustomEvent('json-transform', {
-      detail: {
-        message: data,
-      },
-    });
-    this.dispatchEvent(event);
+    try {
+      const originalValueObj = JSON.parse(this.data);
+      const transformedValue = Function(`"use strict";return (${this.value})`)()(originalValueObj);
+      const transformedValueJson = JSON.stringify(transformedValue);
+      const event = new CustomEvent('json-transform', {
+        detail: {
+          message: transformedValueJson,
+        },
+      });
+      this.dispatchEvent(event);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  handleContent(event) {
+    const { value } = event.detail;
+    this.value = value;
   }
 
   render() {
     return html`
-      <h1>Transform JSON</h1>
+      <h1 class="title">Transform JSON</h1>
       <ace-widget
         placeholder="Write something... Anything..."
         mode="ace/mode/javascript"
@@ -40,9 +79,11 @@ export class JcTransformJson extends LitElement {
         initial-focus
         value=${codeForTransform}
         wrap="true"
+        class="editor"
+        @editor-content=${this.handleContent}
       >
       </ace-widget>
-      <button @click=${this.__transformJson}>Transform</button>
+      <button class="action-button" @click=${this.__transformJson}>Transform</button>
     `;
   }
 }
